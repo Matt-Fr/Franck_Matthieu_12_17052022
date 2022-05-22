4c01ffe9074e9850fcdd53420b630d4f8f1aedf3
Matthieu FRANCK <matthieu_franck@orange.fr>
2022-05-22 13:00:24 +0200

    added links

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
-import Header from "./components/Header";
-import NutritionBloc from "./components/NutritionBloc";
-import SideNav from "./components/SideNav";
-import { USER_MAIN_DATA } from "./data";
-import caloriesIcon from "./assets/calories-icon.svg";
-import proteinIcon from "./assets/protein-icon.svg";
-import carbsIcon from "./assets/carbs-icon.svg";
-import fatIcon from "./assets/fat-icon.svg";
-import Barcharts from "./components/Barcharts";
-import Linechart from "./components/Linechart";
-import Radarchart from "./components/Radarchart";
-import Radialbarchart from "./components/Radialbarchart";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SharedLayout from "./pages/SharedLayout";
+import Profil from "./pages/Profil";
+import Home from "./pages/Home";
 
 function App() {
-  const {
-    id,
-    userInfos: { firstName, lastName, age },
-    todayScore,
-    keyData: { calorieCount, proteinCount, carbohydrateCount, lipidCount },
-  } = USER_MAIN_DATA[0];
-
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SharedLayout />}>
-          {/* <Route index element={<Home />}></Route>
-          <Route path="*" element={<Error />}></Route>
-          <Route path="about" element={<About />}></Route>
-          <Route
+          <Route index element={<Home />}></Route>
+          {/* <Route path="*" element={<Error />}></Route> */}
+          <Route path="profil" element={<Profil />}></Route>
+          {/* <Route
             path="/rent/:accommodationId"
             element={<Accommodation />}
           ></Route> */}
---
+++ src/pages/Home.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { USER_MAIN_DATA } from "../data";
+
+const Home = () => {
+  return (
+    <div>
+      {USER_MAIN_DATA.map((person) => {
+        const {
+          id,
+          userInfos: { firstName, lastName },
+        } = person;
+        console.log(person);
+        return (
+          <Link
+            className="userLink"
+            to={`/user/${id}`}
+          >{`${firstName} ${lastName}`}</Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Home;
