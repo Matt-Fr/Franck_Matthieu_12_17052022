5d98ee4a951c548d02c4d89f3bab42f72696e826
Matthieu FRANCK <matthieu_franck@orange.fr>
2022-05-23 12:48:36 +0200

    added backend

--- src/components/Barcharts.js
+++ src/components/Barcharts.js
@@ -12,7 +12,7 @@ import {
 import { USER_ACTIVITY } from "../data";
 
 const Barcharts = () => {
-  const identity = useParams().id;
+  const { id: identity } = useParams();
   const idendityNum = Number(identity);
   const activity = USER_ACTIVITY.find((user) => user.userId === idendityNum);
   const { sessions } = activity;
