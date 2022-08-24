e8d034357cfebd51c7732b2f69acf050ca9876c5
Matthieu FRANCK <matthieu_franck@orange.fr>
2022-08-24 19:30:02 +0200

    updated the user issue in context

--- src/context.js
+++ src/context.js
@@ -5,23 +5,37 @@ import { useEffect } from "react";
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
+  const [id, setId] = useState("12");
+  const [global, setGlobal] = useState({});
   const [data, setData] = useState({});
+  const [activity, setActivity] = useState({});
   const fetchData = async () => {
     try {
-      const response = await axios("http://localhost:3001/user/18");
+      const response = await axios(`http://localhost:3001/user/${id}`);
       const mainData = response.data;
 
+      const response2 = await axios(
+        `http://localhost:3001/user/${id}/activity`
+      );
+      const dataActivity = response2.data;
+
       setData(mainData);
+      setActivity(dataActivity);
+
+      setGlobal({ mainData: mainData, dataActivity: dataActivity });
     } catch (error) {
       console.log(error);
     }
   };
+  // peut être utiliser un useState pour l'id
 
   useEffect(() => {
-    fetchData();
+    fetchData("18");
   }, []);
 
   console.log(data);
+  console.log(activity);
+  console.log(global);
 
   return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
 };
