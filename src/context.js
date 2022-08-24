afb99219cd60eda5709750c986337db8d5da03af
Matthieu FRANCK <matthieu_franck@orange.fr>
2022-08-24 19:03:59 +0200

    deleted useless code

--- src/context.js
+++ src/context.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useCallback, useContext, useState } from "react";
+import React, { useContext, useState } from "react";
 import { useEffect } from "react";
 
 const AppContext = React.createContext();
