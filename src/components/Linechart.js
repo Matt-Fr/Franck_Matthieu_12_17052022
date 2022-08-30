8fd82eb36b4df845a9c2deaeff316143e520605f
Matthieu FRANCK <matthieu_franck@orange.fr>
2022-08-30 15:20:57 +0200

    improved linechart

--- src/components/Linechart.js
+++ src/components/Linechart.js
@@ -13,6 +13,20 @@ const Linechart = () => {
   const { globalDataUser } = useGlobalContext() || {};
   const { sessionsTime } = globalDataUser || {};
 
+  console.log(sessionsTime);
+
+  if (sessionsTime) {
+    for (let x = 0; x < sessionsTime.length; x++) {
+      sessionsTime[0].day = "L";
+      sessionsTime[1].day = "M";
+      sessionsTime[2].day = "M";
+      sessionsTime[3].day = "J";
+      sessionsTime[4].day = "V";
+      sessionsTime[5].day = "S";
+      sessionsTime[6].day = "D";
+    }
+  } else return "";
+
   return (
     <LineChart
       width={258}
@@ -23,29 +37,45 @@ const Linechart = () => {
       <CartesianGrid strokeDasharray="0 3" />
       <XAxis
         dataKey="day"
-        stroke="rgba(255, 255, 255, 0.6)"
-        axisLine={false}
-        dy={10}
-        tick={{
-          fontSize: 12,
-          fontWeight: 500,
-        }}
         tickLine={false}
+        axisLine={false}
+        tick={{ fill: "#FFFFFF" }}
+        padding={{ left: 15, right: 15 }}
       />
-      <YAxis dataKey="sessionLength" domain={[0, "dataMax + 60"]} hide={true} />
-      <Tooltip />
+      <YAxis
+        hide={true}
+        domain={["dataMin-20", "dataMax+40"]}
+        tickLine={false}
+        axisLine={false}
+      />
+      <Tooltip
+        itemStyle={{
+          color: "black",
+        }}
+        formatter={(value, name, unit) => [value, unit]}
+        labelStyle={{ display: "none" }}
+        cursor={{
+          stroke: "black",
+          strokeOpacity: 0.1,
+          strokeWidth: 70,
+          height: 100,
+        }}
+      />
 
       <Line
         activeDot={{
-          stroke: "rgba(255,255,255, 0.6)",
-          strokeWidth: 2,
+          fill: "white",
+          stroke: "white",
+          strokeOpacity: 0.2,
+          strokeWidth: 15,
           r: 5,
         }}
+        unit="min"
         dataKey="sessionLength"
+        type="monotone"
+        stroke="white"
+        strokeWidth={2}
         dot={false}
-        stroke="rgba(255, 255, 255, 0.6)"
-        strokeWidth={1.5}
-        type={"monotone"}
       />
     </LineChart>
   );
